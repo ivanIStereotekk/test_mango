035854cceab34b53676582a4268f618fdb736076
ewan <ivan.stereotekk@gmail.com>
2023-03-01 01:34:30 +0300

    addded models: Chat Message Reaction

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from typing import AsyncGenerator
-from sqlalchemy import Integer, Column, String, ForeignKey, Text,Boolean
+from sqlalchemy import Integer, Column, String, ForeignKey, Text,Boolean, DateTime
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped,mapped_column
 from typing import List
 
@@ -48,6 +48,37 @@ class Picture(Base):
     def __repr__(self):
         return f"Picture_id={self.id}, user={self.user_id})"
 
+class Chat(Base):
+    __tablename__ = "chat_table"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    users: Mapped[List["User"]] = mapped_column(Integer, ForeignKey("user_table.id"), nullable=True)
+    messages: Mapped[List["Message"]] = mapped_column(Integer, ForeignKey("message_table.id"))
+    created_at: Mapped[str] = mapped_column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Chat_id={self.id}, users={self.users}, created_at={self.created_at})"
+
+
+
+class Reaction(Base):
+    __tablename__ = "reaction_table"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user: Mapped[int] = mapped_column(Integer, ForeignKey("user_table.id"))
+    type: Mapped[str] = mapped_column(String,nullable=True)
+    def __repr__(self):
+        return f"Reaction_id={self.id}, user={self.user}, type={self.type}"
+
+
+
+class Message(Base):
+    __tablename__ = "message_table"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author: Mapped[int] = mapped_column(Integer, ForeignKey("user_table.id"), nullable=False)
+    body: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[str] = mapped_column(Text, nullable=False)
+    reactions: Mapped[List["Reaction"]] = relationship(back_populates="type")
+    def __repr__(self):
+        return f"Message_id={self.id}, author={self.author}, created_at={self.created_at})"
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
