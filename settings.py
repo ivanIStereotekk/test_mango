7cceb5423af8bc4d0365de3b2f5ffe52eaf46af9
ewan <ivan.stereotekk@gmail.com>
2023-02-28 21:08:11 +0300

    fasapi users - added and works

---
+++ settings.py
@@ -0,0 +1,20 @@
+from dotenv import dotenv_values
+import pathlib
+
+# ROOT DIRECTORY OF THE PROJECT
+
+root_dir = pathlib.Path(__file__).parent.resolve()
+
+# DOTENV FILE
+config = dotenv_values(f"{root_dir}/.env")
+
+# PARAMS FOR THE DATABASE
+PG_USER = config["POSTGRES_USER"]
+PG_PASS = config["POSTGRES_PASSWORD"]
+PG_HOST = config["POSTGRES_HOST"]
+PG_PORT = config["POSTGRES_PORT"]
+PG_DB_NAME = config["POSTGRES_DB_NAME"]
+# Authentication secret key
+AUTH_SECRET_KEY = config["AUTH_SECRET_KEY"]
+
+print(config.values())
